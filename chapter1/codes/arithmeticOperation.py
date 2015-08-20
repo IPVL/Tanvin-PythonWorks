7d53c5d2b8d9f29cc510a36f3d0214f6663075a6
panthotanvir <panthotanvir@gmail.com>
2015-08-20 11:14:57 +0600

    Arithmetic operation and Complex number using modules as extension

---
+++ chapter1/codes/arithmeticOperation.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python 
+
+# Basic python 
+print "This program consists of basic arithmetic operations taking input from user"
+
+print "Addition of two integer 100 & 300: ", 100 + 300
+
+print "Subtraction of two integer 100 & 300: ", 100 - 300
+
+print "Multiplication of two integer 100 & 300: ", 100 * 300
+
+print "Division of two integer 100 & 300 without using floating point: ", 100 / 300
+
+print "Division of two integer 100 & 300 using floating point: ", 100.0 / 300
+
+print "Mod of two integer 100 & 300 using floating point: ", 100.0 % 300
+
+print "Large number: ", 1000000000000000000 
+
+print "Hex number of 0xAF :", 0xAF
+
+print "Octal number of 010 :", 010
+
---
+++ chapter1/codes/complexNumber.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+#  Basic complex number and math module
+
+import cmath 
+import math 
+
+
+print "squre root of -1 :", cmath.sqrt(-1)
+
+print "complex number operation like (1+3j) * (9+4j) : ", (1+3j) * (9+4j)
+
+print "Floor operation of 32.9 : ", math.floor(32.9)
