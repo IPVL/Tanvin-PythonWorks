2c71452f82875fbf03c18e897fe1f9a460b2fedb
panthotanvir <panthotanvir@gmail.com>
2015-08-20 12:56:26 +0600

    Difference betwwen input and raw input

---
+++ chapter1/codes/inputRawinput.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python 
+
+#raw input and input difference
+
+# name = input("What is your name? ")
+# print "Hello, " + name + "!"
+
+name = raw_input("What is your name? ")
+print "Hello, " + name + "!"
