e9c6f27d27cc3d2809ce5d8018bd4d47357fe3a9
panthotanvir <panthotanvir@gmail.com>
2015-08-20 12:45:50 +0600

    String Manupulation

---
+++ chapter1/codes/stringManupulation.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# String manupulation
+
+#using repr
+print "Using repr for Hello, world!:", repr("Hello, world!")
+print "Using repr for 10000L : ", repr(10000L)
+
+#using str 
+print "Using str for Hello, world!:", str("Hello, world!")
+print "Using str for 10000L : ", str(10000L)
